a9b59a6889124fe7a9751cfac4d869ae05efdbf5
jgonzalez652 <jgonzalez652@gmail.com>
2025-08-25 14:36:13 +1000

    minor fix

--- Helpers/graphml_exporter.py
+++ Helpers/graphml_exporter.py
@@ -2,8 +2,13 @@ import os
 import networkx as nx
 
 def export_graphml(graph, output_dir, filename):
-    """
-    Export the NetworkX graph to a GraphML file.
-    """
+    # Set node labels
+    for node in graph.nodes:
+        graph.nodes[node]['label'] = str(node)
+    # Set edge labels
+    for u, v, data in graph.edges(data=True):
+        label = f"{data.get('local_interface', '')} ↔ {data.get('neighbor_interface', '')}"
+        graph.edges[u, v]['label'] = label
+    # Export to GraphML
     path = os.path.join(output_dir, f"{filename}.graphml")
-    nx.write_graphml(graph, path, encoding='utf-8')
+    nx.write_graphml(graph, path)
